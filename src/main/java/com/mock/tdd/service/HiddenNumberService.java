ac31f988d85df890c45495ee10c0f00f9e39742e
jenkins <shan.sankara-pandian-muthu@capgemini.com>
2019-01-04 16:58:54 +0000

    init

---
+++ src/main/java/com/mock/tdd/service/HiddenNumberService.java
@@ -0,0 +1,12 @@
+package com.mock.tdd.service;
+
+public interface HiddenNumberService {
+
+    public int addition(int a, int b);
+
+    public int multiply(int a, int b);
+
+    public int subtract(int a, int b);
+
+    public int findHiddenNumber(int total);
+}
---
+++ src/test/java/com/mock/tdd/HiddenNumberServiceTest.java
@@ -0,0 +1,47 @@
+package com.mock.tdd;
+
+import com.mock.tdd.service.HiddenNumberService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HiddenNumberServiceTest {
+
+    private HiddenNumberService hiddenNumberService;
+
+    @Before
+    public void init(){
+        hiddenNumberService = mock(HiddenNumberService.class);
+    }
+
+    @Test
+    public void addtionTest() {
+        when(hiddenNumberService.addition(10,2)).thenReturn(12);
+        assertEquals(12, hiddenNumberService.addition(10,2));
+    }
+
+    @Test
+    public void multiplyTest(){
+        when(hiddenNumberService.multiply(10,2)).thenReturn(20);
+        assertEquals(20, hiddenNumberService.multiply(10,2));
+    }
+
+    @Test
+    public void subtractTest(){
+        when(hiddenNumberService.subtract(10,2)).thenReturn(8);
+        assertEquals(8, hiddenNumberService.subtract(10,2));
+    }
+
+    @Test
+    public void findHiddenNumberTest(){
+        when(hiddenNumberService.findHiddenNumber(24)).thenReturn(2);
+        assertEquals(2, hiddenNumberService.findHiddenNumber(24));
+    }
+
+}
