bc1d132da1f032f593207e8573b80780f1941361
jenkins <shan.sankara-pandian-muthu@capgemini.com>
2019-01-05 16:25:11 +0000

    test doubles and sut execution'

--- src/test/java/com/mock/tdd/service/impl/HiddenNumberServiceImplTest.java
+++ src/test/java/com/mock/tdd/service/impl/HiddenNumberServiceImplTest.java
@@ -4,9 +4,13 @@ import com.mock.tdd.service.HiddenNumberService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HiddenNumberServiceImplTest {
@@ -18,11 +22,24 @@ public class HiddenNumberServiceImplTest {
     @Before
     public void init(){
         hiddenNumberService = mock(HiddenNumberService.class);
+        sut = new HiddenNumberServiceImpl();
     }
 
+
     @Test
-    public  void findHiddenNumberTest(){
-        sut = new HiddenServiceImpl();
+    public void testCalculate(){
+        hiddenNumberService.multiply(10,2);
+        hiddenNumberService.addition(10,2);
+        hiddenNumberService.subtract(10,2);
+        hiddenNumberService.findHiddenNumber(24);
+
+        sut.calculate(10,2);
+
+        assertEquals(2, sut.calculate(10,2));
+        verify(hiddenNumberService).multiply(10,2);
+        verify(hiddenNumberService).addition(10,2);
+        verify(hiddenNumberService).subtract(10,2);
+        verify(hiddenNumberService).findHiddenNumber(24);
     }
 
 }
