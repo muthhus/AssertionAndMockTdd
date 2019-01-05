2082bc50dbadd5344cb9f4f121a98919c3c4db09
jenkins <shan.sankara-pandian-muthu@capgemini.com>
2019-01-05 20:26:59 +0000

    set parameters for tdd

--- src/test/java/com/mock/tdd/service/impl/HiddenNumberServiceImplTest.java
+++ src/test/java/com/mock/tdd/service/impl/HiddenNumberServiceImplTest.java
@@ -1,18 +1,17 @@
 package com.mock.tdd.service.impl;
 
 import com.mock.tdd.service.HiddenNumberService;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.mockito.runners.MockitoJUnitRunner;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class HiddenNumberServiceImplTest {
 
     private HiddenNumberService hiddenNumberService;
@@ -27,19 +26,31 @@ public class HiddenNumberServiceImplTest {
 
 
     @Test
-    public void testCalculate(){
-        hiddenNumberService.multiply(10,2);
-        hiddenNumberService.addition(10,2);
-        hiddenNumberService.subtract(10,2);
-        hiddenNumberService.findHiddenNumber(24);
+    @Parameters({"10, 2, 24, 2", "10, 5, 60, 5", "10, 9, 108, 9"})
+    public void testCalculate(int firstNumber, int guessedNumber, int total, int predictedNumber){
+        hiddenNumberService.multiply(firstNumber,guessedNumber);
+        hiddenNumberService.addition(firstNumber,guessedNumber);
+        hiddenNumberService.subtract(firstNumber,guessedNumber);
+        hiddenNumberService.findHiddenNumber(total);
 
-        sut.calculate(10,2);
+        sut.calculate(firstNumber,guessedNumber);
 
-        assertEquals(2, sut.calculate(10,2));
-        verify(hiddenNumberService).multiply(10,2);
-        verify(hiddenNumberService).addition(10,2);
-        verify(hiddenNumberService).subtract(10,2);
-        verify(hiddenNumberService).findHiddenNumber(24);
+        assertEquals(predictedNumber, sut.calculate(firstNumber,guessedNumber));
+        verify(hiddenNumberService).multiply(firstNumber,guessedNumber);
+        verify(hiddenNumberService).addition(firstNumber,guessedNumber);
+        verify(hiddenNumberService).subtract(firstNumber,guessedNumber);
+        verify(hiddenNumberService).findHiddenNumber(total);
+    }
+
+    
+    private Object[] parameterForTestCalculate() {
+        return new Object[] {
+                new Object[] {10, 2, 24, 2},
+                new Object[] {10, 4, 48, 4},
+                new Object[] {10, 7, 84, 7},
+                new Object[] {10, 8, 96, 8},
+                new Object[] {10, 9, 108, 9}
+        };
     }
 
 }
